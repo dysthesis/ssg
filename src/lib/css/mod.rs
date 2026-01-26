cba7b8790060f21c23af4d312421342998e6869c
Dysthesis <antheoraviel@protonmail.com>
2026-01-26 23:56:27 +1100

    Oh god please go back

--- src/lib/css/mod.rs
+++ src/lib/css/mod.rs
@@ -1,10 +1,5 @@
 use std::{fs, path::Path};
 
-use crate::transformer::code_block::highlight_css;
-
 pub fn build_css(css_path: &Path) -> color_eyre::Result<String> {
-    let mut raw = fs::read_to_string(css_path)?;
-    raw.push('\n');
-    raw.push_str(highlight_css());
-    Ok(raw)
+    fs::read_to_string(css_path).map_err(Into::into)
 }
